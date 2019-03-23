126b461f77b263792482044f78328cc928cf471e
Danny Andres <dannyandres@MacBook-Pro-de-Danny.local>
2019-03-23 15:16:55 -0300

    start

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        dflgjkhdl
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+        ],
+        data() {
+            return {
+            };
+        },
+        mounted() {
+        },
+        created() {
+        },
+        methods: {
+        },
+        watch: {
+        },
+        computed: {
+        }
+    }
+</script>
